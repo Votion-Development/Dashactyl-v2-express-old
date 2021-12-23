34875bcf95561fd3a5f7ce41912ef5b1e099bd03
Jamie_ <volcanomonster07@gmail.com>
2021-12-23 18:21:09 +0000

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Landing.module.css'
+
+const Landing: NextPage = () => {
+  return (
+    <div className={styles.container}>
+    </div>
+  )
+}
+
+export default Landing
