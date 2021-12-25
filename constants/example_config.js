26cc60a3c8c5673234aa813433219efdee3d67d6
Jamie_ <volcanomonster07@gmail.com>
2021-12-25 23:45:01 +0000

    Update example_config.js

--- constants/example_config.js
+++ constants/example_config.js
@@ -1,16 +1,19 @@
 const config = {
-    dashboardName: "Dashactyl",
-    web: {
-      url: "",
-      secret: ""
-    },
-    discord: {
-        id: "", // The OAuth2 application ID
-        secret: "", // The OAuth2 Application Secret
-        callbackpath: ""
-    },
-    discordUrl: "",
-  };
-  
-  export default config;
-  
+  dashboardName: "", // Your dashboards name
+  web: {
+      url: "", // The URL of your dashboard
+      secret: "" // Make this very secure and random
+  },
+  discord: {
+      id: "", // The OAuth2 application ID
+      secret: "", // The OAuth2 Application Secret
+      callbackpath: "", // Callback URL from discord
+      token: "" // Discord bot token
+  },
+  discordUrl: "", // Oauth URL
+  db: {
+      connectionString: "" // MongoDB Connection String
+  }
+};
+
+export default config;
