60f6c6e42d95d99c9211df8074b30fd541de2202
Jamie_ <volcanomonster07@gmail.com>
2021-12-26 00:28:00 +0000

    Update [...nextauth].js

--- pages/api/[...nextauth].js
+++ pages/api/[...nextauth].js
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/discord"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "lib/mongodb"
 
