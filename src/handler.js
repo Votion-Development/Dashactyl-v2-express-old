656bae4da3138d2e80fdc7414948ecc123a55585
devnote-dev <pokeraven4@gmail.com>
2022-01-06 01:28:38 +0000

    refactor(*): replace pages with new handler file

---
+++ src/handler.js
@@ -0,0 +1,79 @@
+const { Router } = require('express');
+const util = require('./util');
+const getUserResources = require('./functions/getUserResources');
+
+const router = Router();
+const settings = util.loadSettings();
+const { pages } = util.loadPages(settings.website.theme);
+
+router.get('*', async (req, res) => {
+    if (req.url === '/') return res.redirect('/login');
+
+    const variables = {};
+    const path = req.url.slice(1);
+    const page = pages[path];
+    if (!page) return res.status(404).render(
+        pages.error404.file,
+        { data: req.session.data, variables }
+    );
+
+    switch (page.permission) {
+        case 0: break;
+        case 1:{
+            if (!req.session.data?.user_info) return res.redirect('/login');
+            break;
+        }
+        case 2:{
+            if (!req.session.data?.panel_info) return res.redirect('/login');
+            if (!req.session.data.panel_info.root_admin) {
+                return res.status(403).render(pages.error403.file);
+            }
+            break;
+        }
+        default:{
+            console.log(
+                `[WEB] Path: '${path}' permission value is invalid. `+
+                'Permission must be 0 (everyone), 1 (logged in), or 2 (administrator).'
+            )
+            return res.status(500).send(
+                'An error has occured while attempting to load this page. '+
+                'Please contact an administrator to fix this.'
+            );
+        }
+    }
+
+    if (page.permission === 0) return res.render(
+        page.file,
+        { data: req.session.data, variables }
+    );
+
+    const resources = await getUserResources(req);
+    if (resources === 'noPackage') {
+        const serverInvite = settings.discord.invite || 'notSet';
+        return res
+            .status(500)
+            .render(pages.error500.file, {
+                error: 'The package assigned to your accounr does not match any packages in the database.',
+                serverInvite
+            });
+    }
+
+    variables.variables = req.session.variables;
+    variables.data = req.session.data;
+    variables.settings = settings;
+
+    if (!resources) {
+        variables.packageInfo = 0;
+        variables.current = 0;
+        variables.extra = 0;
+        variables.total = 0;
+    } else {
+        Object.assign(variables, resources);
+    }
+    variables.timers = {};
+
+    if (req.session.variables) delete req.session.variables;
+    res.render(page.file, variables);
+});
+
+module.exports = router;
