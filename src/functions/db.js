0cba36f0320abe8736bb700456e39f8a622dce3f
devnote-dev <pokeraven4@gmail.com>
2022-01-06 19:50:59 +0000

    refactor(functions): rewrite db tools

--- src/functions/db.js
+++ src/functions/db.js
@@ -1,250 +1,155 @@
-const {
-    MongoClient
-} = require('mongodb');
-const yaml = require('js-yaml')
-const fs = require("fs")
-const fetch = require("node-fetch");
-const res = require('express/lib/response');
+const fetch = require('node-fetch');
+const { MongoClient } = require('mongodb');
+const util = require('../util');
 
-const settings = yaml.load(fs.readFileSync('./src/settings.yml', 'utf8'))
-
-const client = new MongoClient(settings.database.connectionuri);
-
+const settings = util.loadSettings();
+const client = new MongoClient(settings.database.connection_uri);
 const db = client.db(settings.database.name);
 
 (async () => {
     await client.connect();
     console.log('Connected to the database.');
 
-    db.listCollections({ name: "users" })
-        .next(function (err, collinfo) {
-            if (!collinfo) {
-                db.createCollection("users", function (err, res) {
-                    if (err) console.log("There was an error creating the required collections in the mongodb database. Please check the connection URI is correct and that the user has the correct permissions to make collections.");
-                });
-                console.log("The USERS collection has been added to the database.")
-            }
-        });
-    db.listCollections({ name: "sessions" })
-        .next(function (err, collinfo) {
-            if (!collinfo) {
-                db.createCollection("sessions", function (err, res) {
-                    if (err) console.log("There was an error creating the required collections in the mongodb database. Please check the connection URI is correct and that the user has the correct permissions to make collections.");
-                });
-                console.log("The SESSIONS collection has been added to the database.")
-            }
-        });
-    db.listCollections({ name: "coupons" })
-        .next(function (err, collinfo) {
-            if (!collinfo) {
-                db.createCollection("coupons", function (err, res) {
-                    if (err) console.log("There was an error creating the required collections in the mongodb database. Please check the connection URI is correct and that the user has the correct permissions to make collections.");
-                });
-                console.log("The COUPONS collection has been added to the database.")
-            }
-        });
-    db.listCollections({ name: "renewal_timer" })
-        .next(function (err, collinfo) {
-            if (!collinfo) {
-                db.createCollection("renewal_timer", function (err, res) {
-                    if (err) console.log("There was an error creating the required collections in the mongodb database. Please check the connection URI is correct and that the user has the correct permissions to make collections.");
-                });
-                console.log("The RENEWAL_TIMER collection has been added to the database.")
-            }
-        });
-    db.listCollections({ name: "blacklisted" })
-        .next(function (err, collinfo) {
-            if (!collinfo) {
-                db.createCollection("blacklisted", function (err, res) {
-                    if (err) console.log("There was an error creating the required collections in the mongodb database. Please check the connection URI is correct and that the user has the correct permissions to make collections.");
-                });
-                console.log("The BLACKLISTED collection has been added to the database.")
-            }
-        });
-    db.listCollections({ name: "packages" })
-        .next(function (err, collinfo) {
-            if (!collinfo) {
-                db.createCollection("packages", function (err, res) {
-                    if (err) console.log("There was an error creating the required collections in the mongodb database. Please check the connection URI is correct and that the user has the correct permissions to make collections.");
-                });
-                console.log("The PACKAGES collection has been added to the database.")
-            }
-        });
+    const DATABASES = [
+        'users', 'sessions', 'coupons',
+        'renewal_timer', 'blacklisted',
+        'packages'
+    ];
+
+    for (const coll of DATABASES) {
+        db.listCollections({ name: coll })
+            .next((_, data) => {
+                if (!data) {
+                    db.createCollection(coll, (err, _) => {
+                        if (err) console.log(
+                            `There was an error creating the '${coll}' collection in the database. `+
+                            'Please make sure that the connection URI is correct and that the user '+
+                            'has the correct permissions to create collections.'
+                        );
+                    });
+                }
+            });
+    }
 })();
 
+async function fetchAccount(id) {
+    return await db
+        .collection('users')
+        .findOne({ discordID: id });
+}
+
+async function createAccount(data) {
+    const collection = db.collection('users');
+    const user = await collection.findOne({ discordID: data.id });
+    if (user) return;
+
+    let password;
+    if (settings.pterodactyl.generate_password_on_signup) {
+        password = Math.random()
+            .toString(36).substring(2, 15) +
+            Math.random().toString(36).substring(2, 15);
+    }
+
+    const res = await fetch(
+        `${settings.pterodactyl.domain}/api/application/users`, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${settings.pterodactyl.key}`
+            },
+            body: JSON.stringify({
+                username: data.id,
+                email: data.email,
+                first_name: data.username,
+                last_name: data.discriminator,
+                password
+            })
+        }
+    );
+
+    if (res.status === 201) {
+        const json = (await res.json()).attributes;
+        await collection.insertOne({
+            discordID: id,
+            pterodactylID: json.password,
+            coins: 0,
+            package: 'default',
+            memory: 0,
+            disk: 0,
+            cpu: 0,
+            servers: 0,
+            dateAdded: Date.now()
+        });
+
+        json.password &&= password;
+        json.relationships = {
+            servers:{
+                object: 'list',
+                data: []
+            }
+        };
+
+        return json;
+    }
+}
+
+async function deleteAccount(id) {
+    return await db
+        .collection('users')
+        .deleteOne({ discordID: id });
+}
+
+async function checkBlacklisted(id) {
+    const data = await db
+        .collection('users')
+        .findOne({ discordID: id });
+
+    return !!data;
+}
+
+async function getPackages(name = null) {
+    const packages = await db
+        .collection('packages')
+        .find({})
+        .toArray();
+
+    if (!name) return packages;
+    if (name === 'default') return packages.find(p => p.default);
+    return packages.find(p => p.name === name);
+}
+
+async function addPackage(name, memory, disk, cpu, servers, isDefault) {
+    const packages = await getPackages();
+    if (packages.find(p => p.name === name)) return false;
+
+    await db
+        .collection('packages')
+        .insertOne({
+            name,
+            memory,
+            disk,
+            cpu,
+            servers,
+            default: isDefault,
+            dateAdded: Date.now()
+        });
+
+    return true;
+}
+
+async function deletePackage(name) {
+    await db
+        .collection('packages')
+        .deleteOne({ name });
+}
+
 module.exports = {
-    async createAccount(id, email, username, discriminator) {
-        const collection = db.collection("users");
-
-        const filteredDocs = await collection.findOne({
-            discordID: id
-        });
-
-        if (!filteredDocs) {
-            const generated_password = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-
-            const first_name = username
-            const last_name = discriminator
-
-            const account = await fetch(
-                `${settings.pterodactyl.domain}/api/application/users`, {
-                method: 'POST',
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${settings.pterodactyl.key}`
-                },
-                body: JSON.stringify({
-                    username: id,
-                    email: email,
-                    first_name: first_name,
-                    last_name: last_name,
-                    password: settings.pterodactyl.generate_password_on_sign_up ? generated_password : undefined
-                })
-            }
-            )
-
-            if (account.status === 201) {
-                const accountinfo = await account.json()
-
-                await collection.insertOne({
-                    discordID: id,
-                    pterodactylID: accountinfo.attributes.id,
-                    coins: null,
-                    package: null,
-                    memory: null,
-                    disk: null,
-                    cpu: null,
-                    servers: null,
-                    dateadded: Date(),
-                });
-
-                accountinfo.attributes.password = generated_password
-
-                accountinfo.attributes.relationships = {
-                    servers: {
-                        object: 'list',
-                        data: []
-                    }
-                }
-
-                return accountinfo.attributes
-            } else {
-                const accountlistjson = await fetch(
-                    `${settings.pterodactyl.domain}/api/application/users?include=servers&filter[email]=${encodeURIComponent(email)}`, {
-                    method: 'get',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${settings.pterodactyl.key}`
-                    }
-                }
-                )
-
-                const accountlist = await accountlistjson.json()
-                const user = accountlist.data.find(acc => acc.attributes.email === email)
-
-                if (user) {
-                    const userid = user.attributes.id
-
-                    await collection.insertOne({
-                        discordID: id,
-                        pterodactylID: userid,
-                        email: email,
-                        username: username,
-                        coins: 0,
-                        package: `Not Set`,
-                        memory: 0,
-                        disk: 0,
-                        cpu: 0,
-                        servers: 0,
-                        dateadded: Date(),
-                    });
-
-                    return user.attributes
-                };
-            }
-        }
-    },
-
-    async fetchAccount(id) {
-        const db = client.db("dashactyl");
-        const collection = db.collection("users");
-
-        const filteredDocs = await collection.findOne({
-            discordID: id
-        });
-
-        return filteredDocs;
-    },
-
-    async blacklistStatus(id) {
-        const db = client.db("dashactyl");
-        const collection = db.collection("blacklisted");
-
-        const filteredDocs = await collection.findOne({
-            discordID: id
-        });
-
-        if (!filteredDocs) {
-            return false;
-        } else {
-            return true;
-        }
-    },
-
-    async addPackage(name, memory, disk, cpu, servers, isDefault) {
-        const db = client.db("dashactyl");
-        const collection = db.collection("packages");
-
-        const filteredDocs = await collection.findOne({
-            name: name
-        });
-
-        if (!filteredDocs) {
-            await collection.insertOne({
-                name: name,
-                memory: memory,
-                disk: disk,
-                cpu: cpu,
-                servers: servers,
-                default: isDefault,
-                dateadded: Date(),
-            });
-
-            return true;
-        } else {
-            return false;
-        }
-    },
-
-    async getPackages() {
-        const db = client.db("dashactyl");
-        const collection = db.collection("packages");
-
-        const foundResults = await collection.find({}).toArray();
-
-        return foundResults
-    },
-
-    async getDefaultPackage() {
-        const db = client.db("dashactyl");
-        const collection = db.collection("packages");
-
-        const filteredDocs = await collection.findOne({
-            default: true
-        });
-
-        return filteredDocs;
-    },
-
-    async findPackage(package) {
-        const db = client.db("dashactyl");
-        const collection = db.collection("packages");
-
-        const filteredDocs = await collection.findOne({
-            name: package
-        });
-
-        return filteredDocs;
-    },
+    fetchAccount,
+    createAccount,
+    deleteAccount,
+    checkBlacklisted,
+    getPackages,
+    addPackage,
+    deletePackage
 }
